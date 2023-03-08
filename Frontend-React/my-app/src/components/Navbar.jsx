94985b4ee2988f5b191b19bc974b2bd37b44fbcc
Amirhossein_Daliri <amirdalir0111@gmail.com>
2023-03-08 17:05:02 +0330

    create contacts && contact component, spinner for loading

--- Frontend-React/my-app/src/components/Navbar.jsx
+++ Frontend-React/my-app/src/components/Navbar.jsx
@@ -1,18 +1,18 @@
 import SearchContact from "./contact/SearchContact";
 
-import { Purple, Background } from "../utils/colors";
+import { PURPLE, BACKGROUND } from "../utils/colors";
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-dark navbar-expand-sm shadow-lg"
-        style={{backgroundColor: Background}}>
+        style={{backgroundColor: BACKGROUND}}>
             <div className="container">
                 <div className="row w-100">
                     <div className="col mb">
                         <div className="navbar-brand">
-                            <i className="fa fa-id-badge" style={{color: Purple}}/>
+                            <i className="fa fa-id-badge" style={{color: PURPLE}}/>
                             {"  "}اپلیکیشن مدیریت{" "}
-                            <span style={{color: Purple}}>مخاطبین</span>
+                            <span style={{color: PURPLE}}>مخاطبین</span>
                         </div>
                     </div>
                     <div className="col">
--- Frontend-React/my-app/src/components/contact/SearchContact.jsx
+++ Frontend-React/my-app/src/components/contact/SearchContact.jsx
@@ -1,16 +1,16 @@
-import {Purple} from "../../utils/colors";
+import {PURPLE} from "../../utils/colors";
 
 const SearchContact = () => {
 
     return (
         <div className="input-group mx-2 w-75" dir="ltr">
-            <span className="input-group-text" id="basic-addon1" style={{backgroundColor: Purple}}>
+            <span className="input-group-text" id="basic-addon1" style={{backgroundColor: PURPLE}}>
                 <i className="fa fa-search" />
             </span>
             <input
                 type="text"
                 dir="rtl"
-                style={{borderColor: Purple}}
+                style={{borderColor: PURPLE}}
                 className="form-control"
                 placeholder="جستجوی مخاطب"
                 aria-label="Search"
